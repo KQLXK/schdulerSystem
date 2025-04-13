4c9cfbaee70c4b5e1a22fe479e63c6a02b9273bc
KQLXK <yuchao1726199979@163.com>
2025-04-13 16:10:09 +0800

    查询班级课表

--- services/table/querybyclass.go
+++ services/table/querybyclass.go
@@ -7,6 +7,7 @@ import (
 // GetClassScheduleBySemester 查询班级课表
 func GetClassScheduleBySemester(classID string, semester string) ([]models.ScheduleResult, error) {
 	// 假设 ScheduleResult 结构体中有一个字段存储学期信息
+	var results []models.ScheduleResult
 	scheduleResults, err := models.GetAllScheduleResults()
 	if err != nil {
 		return nil, err
@@ -15,11 +16,11 @@ func GetClassScheduleBySemester(classID string, semester string) ([]models.Sched
 	// 过滤出符合条件的排课结果
 	for _, result := range scheduleResults {
 		if IsClassInClassList(classID, result.ClassIDs) && result.Schedule.Semester == semester {
-			scheduleResults = append(scheduleResults, result)
+			results = append(results, result)
 		}
 	}
 
-	return scheduleResults, nil
+	return results, nil
 }
 
 // 判断class是否在[]class中的函数
